e2a861ccfe0ea62194c10d2d1671a71eccac1207
Vin Bui <vdb23@cornell.edu>
2023-11-10 02:20:01 -0500

    TA Read Access

--- src/create_repo.py
+++ src/create_repo.py
@@ -33,18 +33,21 @@ def create_repo(netid, assignment):
 
     # Create repository in organization and add collaborators
     organization = g.get_organization(ORG_NAME)
+    team = organization.get_team_by_slug("tas")
     try:
         if netid_2 is None:
             repo_name = f"{netid_1}-{assignment}"
             organization.create_repo(repo_name, private=True)
             repo = organization.get_repo(repo_name)
             repo.add_to_collaborators(netid_1, permission="push")
+            team.add_to_repos(repo)
         else:
             repo_name = f"{netid_1}-{netid_2}-{assignment}"
             organization.create_repo(repo_name, private=True)
             repo = organization.get_repo(repo_name)
             repo.add_to_collaborators(netid_1, permission="push")
             repo.add_to_collaborators(netid_2, permission="push")
+            team.add_to_repos(repo)
     except:
         print(f"Error creating repository for {netid}")
 
